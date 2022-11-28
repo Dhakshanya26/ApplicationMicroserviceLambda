6f6998f51c71c50327f61f6b6a4c4e54b46cd6e7
Sudha Arumugam <arumsudu@amazon.co.uk>
2022-11-28 18:12:39 +0000

    added post

--- createloanapplication/Loan_Application/app.py
+++ createloanapplication/Loan_Application/app.py
@@ -1,14 +1,37 @@
 import json
+import requests
+import uuid
+import boto3
 
-# import requests
-
-
+client = boto3.client('dynamodb')
 def lambda_handler(event, context):
-      
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "loan application test",
-            # "location": ip.text.replace("\n", "")
-        }),
+
+    data = client.put_item(
+        TableName = 'applications',
+        Item = {
+            'id': {
+                'S':  uuid.uuid1()
+            },
+            'firstname': {
+                'S': event['firstname']
+            },
+            'lastname': {
+                'S': event['lastname']
+            }
+        }
+    )
+
+    data2 = client.scan(
+        TableName = 'applications'
+    )
+
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(data2),
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
     }
+
+    return response
