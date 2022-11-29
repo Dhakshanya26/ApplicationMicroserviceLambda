2929c1bcf6f55691575756406df520fe7409faee
Sudha Arumugam <arumsudu@amazon.co.uk>
2022-11-29 19:42:11 +0000

    updated lambda

--- createloanapplication/Loan_Application/app.py
+++ createloanapplication/Loan_Application/app.py
@@ -6,18 +6,25 @@ import boto3
 client = boto3.client('dynamodb')
 def lambda_handler(event, context): 
     input = json.loads(event['body'])
+    applicationId = str(uuid.uuid1())
     data = client.put_item(
         TableName = 'applications',
         Item = {
-            'id': {
-                'S':  str(uuid.uuid1())
+            'Id': {
+                'S':  applicationId
             },
             'firstname': {
                 'S': input['firstname']
             },
             'lastname': {
                 'S': input['lastname']
-            }
+            },
+            'emailAddress': {
+                'S': input['emailAddress']
+            },
+            'loanAmount': {
+                'S': input['loanAmount']
+            } 
         }
     )
 
@@ -25,6 +32,19 @@ def lambda_handler(event, context):
         TableName = 'applications'
     )
 
+    eventClient = boto3.client('events')
+    detailJsonString = '{"Id":"applicationId"}'
+
+    publishedEventResponse = eventClient.put_events(
+        Entries=[
+            {
+                'Source':'user-event',
+                'DetailType':'user-preferences',
+                'Detail':detailJsonString,
+                'EventBusName':'LoanApplicationEventBus' 
+            }
+        ]
+    )
     response = {
         'statusCode': 200,
         'body': json.dumps(data2),
